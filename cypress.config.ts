a5532c72f97fb59e750ee5e1640b5b02c11bcea8
Waed Sammar <waedsammar12@gmail.com>
2025-10-05 15:36:33 +0300

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
+  },
+  viewportWidth: 1728,
+  viewportHeight: 1117,
+});
