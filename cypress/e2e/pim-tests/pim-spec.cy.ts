4f8e3a32662c0563c23bfd0f9b9f1948b0b1d0bf
Waed Sammar <waedsammar12@gmail.com>
2025-10-16 20:37:43 +0300

    refactor! apply cleanup

--- cypress/e2e/pim-tests/pim-spec.cy.ts
+++ cypress/e2e/pim-tests/pim-spec.cy.ts
@@ -6,6 +6,7 @@ import { IEmployeeInfo } from "../../support/types/employee";
 
 describe("Employee management - Add and Save Test Cases", () => {
   let employeeMockData: IEmployeeInfo, employeeInfo: IEmployeeInfo;
+  let employeeNum: number[] = [];
 
   before(() => {
     cy.fixture("employee-page-mock").then((addEmployeeData) => {
@@ -14,6 +15,7 @@ describe("Employee management - Add and Save Test Cases", () => {
   });
 
   beforeEach(() => {
+    employeeNum = [];
     cy.login();
     employeeInfo = {
       ...employeeMockData,
@@ -32,21 +34,25 @@ describe("Employee management - Add and Save Test Cases", () => {
     APIsHelper.interceptGetEmployeeDetailsRequest(createLoadPersonalDetails);
     PIMPage.clickSave();
     APIsHelper.waitForApiResponse(createLoadPersonalDetails);
+
+    PIMPageHelper.getEmpNumberByEmployeeId(employeeInfo.employeeId).then(
+      (empNumber) => {
+        employeeNum.push(empNumber);
+      }
+    );
   });
 
   it("Adding five employees via API", () => {
     for (let i = 0; i < 5; i++) {
-      PIMPageHelper.createEmployeeViaAPI(employeeInfo);
+      PIMPageHelper.createEmployeeViaAPI(employeeInfo).then((response) => {
+        employeeNum.push(response.body.data.empNumber);
+      });
     }
   });
 
   afterEach(() => {
     cy.logout();
     cy.login();
-    PIMPageHelper.getEmpNumberByEmployeeId(employeeInfo.employeeId).then(
-      (empNumber) => {
-        PIMPageHelper.deleteUsers([empNumber!]);
-      }
-    );
+    PIMPageHelper.deleteUsers(employeeNum);
   });
 });
